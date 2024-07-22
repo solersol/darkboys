c294f7bb8ed13eb32274052d3967b3e7ca8b22f9
solersol <ttpage1tk@gmail.com>
2024-07-22 18:38:10 +0300

    Vfd

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,10 +3,10 @@ const Hero = () => {
     <section id="home" className="w-full flex flex-col justify-center items-center min-h-screen   bg-cover bg-center">
       <h1 className="flex justify-center font-anton  text-[70px] mb-0 text-[#fff]">DARK BOYS CLUB</h1>
       {/* <h2 className="flex justify-center font-anton  text-[28px] mb-4 mt-0 text-[#fff]">DARK BOYS CLUB</h2> */}
-      <a href="https://pump.fun/"><button className="mt-6 bg-[#2e1515] text-[#fff] rounded-full px-10 py-4 hover:bg-[#f0ff00] font-anton  text-[24px]">
+      <a href="https://pump.fun/AG16yKveLaRn88Cy6mrafrtjHJV1sF5Uf6NWPo4Fpump"><button className="mt-6 bg-[#2e1515] text-[#fff] rounded-full px-10 py-4 hover:bg-[#f0ff00] font-anton  text-[24px]">
         BUY $DBOYZ
       </button></a>
-      <h1 className="flex justify-center font-anton text-[35px] mt-6 mb-96 text-center px-4 break-words text-[#fff]">CA: INCOMING...  </h1>
+      <h1 className="flex justify-center font-anton text-[35px] mt-6 mb-96 text-center px-4 break-words text-[#fff]">CA: AG16yKveLaRn88Cy6mrafrtjHJV1sF5Uf6NWPo4Fpump  </h1>
 
     </section>
   );
